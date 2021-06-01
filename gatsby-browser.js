013ec1c262c62ddcb725c8575acc3212d185d4bf
Allen <allen0321.lin@ehsn.com.tw>
2021-06-01 23:40:42 +0800

    完成首頁上方區塊

---
+++ gatsby-browser.js
@@ -0,0 +1,6 @@
+﻿const React = require('react')
+const Layout = require('./src/components/Layout').default
+
+exports.wrapPageElement = ({ element, props }) => {
+  return <Layout {...props}>{element}</Layout>
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import logo from '../assets/images/logo.svg'
 import { FaAlignRight } from 'react-icons/fa'
 import pageLinks from '../constants/links'
-import { Link } from 'gatsby'
+
 const Navbar = () => {
-  return <h2>navbar component</h2>
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/">
+            <img src={logo} alt="web dev" />
+          </Link>
+          <button type="button" className="toggle-btn">
+            <FaAlignRight />
+          </button>
+        </div>
+        <div className="nav-links">
+          {pageLinks.map(link => {
+            return (
+              <Link to={link.url} key={link.id}>
+                {link.text}
+              </Link>
+            )
+          })}
+        </div>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
