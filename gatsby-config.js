abdb0633c56d82bd6968de63256021fb4db9a2fb
Allen <allen0321.lin@ehsn.com.tw>
2021-06-01 14:40:39 +0800

    Init

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,17 @@
 module.exports = {
-  siteMetadata: {
-    title: "portfolio",
-  },
-  plugins: [],
+  /* Your site config here */
+
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+  ],
 };
