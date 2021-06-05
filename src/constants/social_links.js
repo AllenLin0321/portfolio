0970000800eb591ddb0e4c207677f8c77cbf098d
Allen <allen0321.lin@ehsn.com.tw>
2021-06-05 23:09:04 +0800

    添加履歷

--- src/constants/social_links.js
+++ src/constants/social_links.js
@@ -1,16 +1,21 @@
 import React from 'react'
-import { FaFacebookSquare, FaLinkedin } from 'react-icons/fa'
+import { FaFacebookSquare, FaLinkedin, FaRegFilePdf } from 'react-icons/fa'
 
 const data = [
   {
     id: 1,
     icon: <FaFacebookSquare className="social-icon"></FaFacebookSquare>,
-    url: 'https://www.twitter.com',
+    url: 'https://www.facebook.com/yilun.lin.56',
   },
   {
     id: 2,
     icon: <FaLinkedin className="social-icon"></FaLinkedin>,
     url: 'https://www.linkedin.com/in/yi-lun-lin',
+  },
+  {
+    id: 3,
+    icon: <FaRegFilePdf className="social-icon" />,
+    url: 'https://drive.google.com/file/d/1pi5O2t1oOT3nyVrsYJOuernl2mJIsghS/view?usp=sharing',
   },
 ]
 
