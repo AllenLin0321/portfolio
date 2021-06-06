5f047aeb63300277ee5d5444df2e10136a8332be
Allen <allen0321.lin@ehsn.com.tw>
2021-06-06 16:29:19 +0800

    新增GitHub Social Link

--- src/constants/social_links.js
+++ src/constants/social_links.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaFacebookSquare, FaLinkedin, FaUserTie } from 'react-icons/fa'
+import { FaFacebookSquare, FaLinkedin, FaGithubSquare, FaUserTie } from 'react-icons/fa'
 
 const data = [
   {
@@ -13,7 +13,12 @@ const data = [
     url: 'https://www.linkedin.com/in/yi-lun-lin',
   },
   {
-    id: 3,
+    id: 2,
+    icon: <FaGithubSquare className="social-icon"></FaGithubSquare>,
+    url: 'https://github.com/AllenLin0321',
+  },
+  {
+    id: 4,
     icon: <FaUserTie className="social-icon" />,
     url: 'https://drive.google.com/file/d/1pi5O2t1oOT3nyVrsYJOuernl2mJIsghS/view?usp=sharing',
   },
